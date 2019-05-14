2fe5cf05f107177f88cffdc5bc6fc89eecea6615
Kazaki Shirokane <hyper.munn@gmail.com>
2019-05-14 09:39:33 +0800

    [Phase 2] Insert handshakers and responser

--- src/plugin/readyEvent/handShake.js
+++ src/plugin/readyEvent/handShake.js
@@ -13,12 +13,13 @@ module.exports = (client) => {
       .then(res => {
         // If got the hand shake to the server
         if (res.status === 200) {
+          timer.stop();
           client.log.info(`[HANDSHAKE] Handshake received! (${timer.time()} ms)`);
         }
       })
       .catch(err => {
+        timer.stop();
         client.log.error(`[HANDSHAKE] Handshake Error! (${timer.time()} ms)\n\t${err.message}`);
       })
-    timer.stop();
   }, 60000);
 }
--- src/plugin/readyEvent/uptime.js
+++ src/plugin/readyEvent/uptime.js
@@ -9,22 +9,9 @@ const { Client } = require('../../Bot');
  */
 module.exports = (client) => {
   if (process.env.DEV) return;
+
   app.get('/', (request, response) => {
     response.sendStatus(200);
   });
   app.listen(process.env.PORT);
-  setInterval(() => {
-    let timer = new TimerMachine();
-    timer.start();
-    axios(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`)
-      .then(res => {
-        if (res.status === 200) {
-          client.log.info(`[PING] Ping received! (${timer.time()} ms)`);
-        }
-      })
-      .catch(err => {
-        client.log.info(`[PING] Ping not received! (${timer.time()} ms)\n\t${err.message}`);
-      })
-    timer.stop();
-  }, 60000);
 }
